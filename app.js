237730b5affa4ed0aad405ee0ca10f6a7d149c3e
kim1037 <pan913063@gmail.com>
2023-03-27 15:49:47 +0800

    feat: set project init

---
+++ app.js
@@ -0,0 +1,16 @@
+//載入相關模組
+const express = require("express");
+const exphbs = require("express-handlebars");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render('index')
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on http://localhost:${PORT}`);
+});
