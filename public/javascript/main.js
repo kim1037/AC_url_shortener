71649f19af6972841efc3815b03ad4f83c919234
kim1037 <pan913063@gmail.com>
2023-03-28 15:57:44 +0800

    feat: add bootswatch

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,10 +1,12 @@
-const copyBtn = document.querySelector('#copy-btn')
+//點擊 copy button 複製內容
+const copyBtn = document.querySelector("#copy-btn");
 
-copyBtn.addEventListener('click', function onCopyBtnClicked(event){
-  const newUrl = document.querySelector('#new-url').value
-  navigator.clipboard.writeText(newUrl).then(()=>{
+copyBtn.addEventListener("click", function onCopyBtnClicked(event) {
+  const newUrl = document.querySelector("#new-url").value;
+  navigator.clipboard.writeText(newUrl).then(() => {
     //copy成功顯示訊息
-    document.querySelector('#copy-message').classList.remove("copy-message-hide")
-  }
-  )
-})
+    document
+      .querySelector("#copy-message")
+      .classList.remove("copy-message-hide");
+  });
+});
